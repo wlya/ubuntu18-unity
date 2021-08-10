06ddde5cd95cd40818086e35d271a402353e1721
WhoamiMiss <diigu@qq.com>
2021-08-10 10:46:31 +0800

    Create ubuntu20.init202108.sh

---
+++ ubuntu20.init202108.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install -y gdb gcc g++ python3 python3-pip build-essential  python3-dev git libssl-dev libffi-dev 
+
+sudo sh -c 'echo "v ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers'
+
+sudo apt install -y fonts-powerline zsh terminator git gcc curl  unity-tweak-tool indicator-multiload unzip easystroke wmctrl xdotool 
+sudo apt install -y ubuntu-unity-desktop 
